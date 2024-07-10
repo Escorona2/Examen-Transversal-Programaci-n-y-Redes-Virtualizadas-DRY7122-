5c9f64f0a2443812f5f91c0cf4251e862fd3ea56
Escorona2 <166894639+Escorona2@users.noreply.github.com>
2024-07-10 17:06:47 -0400

    Add files via upload

---
+++ Integrantes.py
@@ -0,0 +1,11 @@
+# Definimos una lista con los nombres y apellidos de los integrantes del grupo
+integrantes_del_grupo = [
+    "Estefanía Coronado",
+    "Kimberly Valdebenito",
+    "Angelo Carrillo",
+
+]
+
+# Imprimimos cada nombre en la lista
+for integrante in integrantes_del_grupo:
+    print(integrante)
